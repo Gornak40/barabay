efcd0738aeddc518780c2b4c6e3d599d00a81aac
Gornak40 <s-kozelsk@yandex.ru>
2024-12-04 01:01:53 +0300

    feat: mass template progress

--- barabay.sh
+++ barabay.sh
@@ -9,18 +9,63 @@ fi
 
 t=$(cat "$1")
 
+
+# key<tab>value
+getkey(){
+	local k=${1%%$'\t'*}
+	echo -E "$k"
+}
+
+
+# key<tab>value
+getval(){
+	local v=${1#*$'\t'}
+	echo -E "$v"
+}
+
+
+# {{<spaces>key<spaces>}} -> value
+# text,key,value
+repl2(){
+	local t=$(awk '{ gsub("{{[[:space:]]*'"$2"'[[:space:]]*}}", "'"$3"'"); print }' <<< "$1")
+	echo -E "$t"
+}
+
+
+fn="" # section name
+ft="" # section template
+fs="" # section process
+
 while read -r p; do
 
 case "$p" in
 	*$'\t'*)
-		k=${p%%$'\t'*}
-		v=${p#*$'\t'}
-		t=$(awk '{ gsub("{{[[:space:]]*'$k'[[:space:]]*}}", "'$v'"); print }' <<< $t)
+		k=$(getkey "$p")
+		v=$(getval "$p")
+		if [[ "${k:0:1}" == "@" ]]; then
+			fn="$k"
+			ft=$(cat "$v")
+			fs="$ft"
+		else
+			if [[ -z "$fs" ]]; then
+				t=$(repl2 "$t" "$k" "$v")
+			else
+				fs=$(repl2 "$fs" "$k" "$v")
+			fi
+		fi
+		;;
+	"")
+		if [[ ! -z "$fn" ]]; then
+			echo > /dev/null
+			# TODO: fix this :(
+			# t=$(repl2 "$t" "$fn" "$fs")
+		fi
 		;;
 	*)
+		# nothing interesting here
 		;;
 esac
 
 done
 
-cat <<< $t
+cat <<< "$t"
